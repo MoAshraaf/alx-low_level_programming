f4647ca0ac7e8c36590e0fa6c5acde1cc1995679
MoAshraaf <mashraff82@gmail.com>
2023-07-13 08:37:56 -0700

    y

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-
-int main() {
-    puts("\"Programming is like building a multilingual puzzle");
-    return 0;
+/**
+ * main - entry poit
+ * return: always 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
